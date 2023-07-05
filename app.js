4e65bb8e30ca3a4c970c08709ff18c7ff9fd7000
TerryW624 <terrywade634@gmail.com>
2023-07-05 11:11:06 -0700

    feat: Problem 1

--- app.js
+++ app.js
@@ -142,14 +142,23 @@ console.log('mexicanFood from filterExample', mexicanFood)
 function problemOne(){
 
     let results;
+    results = dishes.filter(function(el){
+        console.log(`${el.name} in Dishes: `, el.cuisine)
+        if (el.cuisine === "Vegetarian"){
+            return true
+        } 
+        else {
+            return false
+        }
+    })
 
     return results;
 }
+let vegetarianFood = problemOne();
+console.log("vegetarian food from dishes ", vegetarianFood);
 
 //2. Create a function that will prompt the user to enter a cuisine type and then return all dishes that match that type
 //Filter
-
-
 
 //3. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
